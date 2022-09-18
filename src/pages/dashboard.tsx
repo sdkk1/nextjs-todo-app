d2ff2bae0c2842c949871dc79a26fab252a9f449
Keita <fast.break.baske03@gmail.com>
2022-09-18 23:55:58 +0900

    add: Dashboardページを作成

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from 'next'
+import { LogoutIcon } from '@heroicons/react/solid'
+
+import Layout from '@/components/Layout'
+import { supabase } from '@/utils/supabase'
+
+const Dashboard: NextPage = () => {
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+  return (
+    <Layout title='Dashboard'>
+      <LogoutIcon
+        className='mb-6 h-6 w-6 cursor-pointer text-blue-500'
+        onClick={signOut}
+      />
+    </Layout>
+  )
+}
+
+export default Dashboard
