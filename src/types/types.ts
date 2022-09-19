8ce76d29f624e1a2f569c99dd93013be1c7dbbc5
Keita <fast.break.baske03@gmail.com>
2022-09-19 16:12:01 +0900

    update: 型定義を追加

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,10 +4,23 @@ export type Task = {
   title: string
   user_id?: string
 }
+export type CreatedTask = Omit<Task, 'id' | 'created_at'>
+export type EditedTask = Omit<Task, 'created_at' | 'user_id'>
 
 export type Notice = {
   id: string
   created_at: string
   content: string
   user_id?: string
+}
+export type CreatedNotice = Omit<Notice, 'id' | 'created_at'>
+export type EditedNotice = Omit<Notice, 'created_at' | 'user_id'>
+
+export type State = {
+  editedTask: EditedTask
+  editedNotice: EditedNotice
+  updateEditedTask: (payload: EditedTask) => void
+  updateEditedNotice: (payload: EditedNotice) => void
+  resetEditedTask: () => void
+  resetEditedNotice: () => void
 }
